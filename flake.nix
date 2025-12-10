7743acd4e65bbf95d4b529a7db7aaed9d1e456f4
cedev <cedevserver@gmail.com>
2025-12-10 22:53:07 +0100

    init flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Flake template";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          "x86_64-darwin"
+          "aarch64-darwin"
+        ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            direnv
+            bun
+          ];
+
+          shellHook = ''
+            echo "[✔] OK"
+          '';
+        };
+      });
+    };
+}
